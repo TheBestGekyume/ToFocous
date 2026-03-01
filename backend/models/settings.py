0f0fe448638e3add4003f5057e1a292db2bc8f57
jojobms <jonathanbastosmarques@gmail.com>
2026-03-01 13:58:28 -0300

    modificação nas classes e requirements

--- backend/models/settings.py
+++ backend/models/settings.py
@@ -1,7 +1,16 @@
 from pydantic import BaseModel
 from typing import Optional
+from enum import Enum
 
+class WhichDateUseInCalendar(str, Enum):
+    UseStartDate = "Usar Data de Início"
+    UseDueDate = "Usar Data Limite"
+    UseBoth = "Usar Ambas"
 
+    
 class UpdtSettings(BaseModel):
     use_subtask_time: Optional[bool] = None
     use_subtask_priority: Optional[bool] = None
+    which_date_use_in_calendar: Optional[WhichDateUseInCalendar] = None
+
+
